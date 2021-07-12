221fec287588d7b88d47e6326c1690cf5555c1de
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-07-13 00:01:12 +0530

    Added: Length of two line Compared

--- src/lineComparision.java
+++ src/lineComparision.java
@@ -14,19 +14,33 @@ public class lineComparision {
 		int y1 = sc.nextInt();
 		System.out.print("Enter y2 : ");
 		int y2 = sc.nextInt();
-		
-		double length = 0;
+		System.out.print("Enter x3 : ");
+		int x3 = sc.nextInt();
+		System.out.print("Enter x4 : ");
+		int x4 = sc.nextInt();
+		System.out.print("Enter y3 : ");
+		int y3 = sc.nextInt();
+		System.out.print("Enter y4 : ");
+		int y4 = sc.nextInt();
+	
 		double x = Math.pow((x2-x1),2);
 		double y = Math.pow((y2-y1),2);
+		double a = Math.pow((x4-x3),2);
+		double b = Math.pow((y4-y3),2);
 
-		String obj1 =String.valueOf(x);
-		String obj2 =String.valueOf(y);
-		boolean result = obj1.equals(obj2);
-		if(result == true ){
+		Object length1 = (x+y);
+		Object length2 = (a+b);
+		String obj1 =String.valueOf(length1);
+		String obj2 =String.valueOf(length2);
+		
+		 double result = obj1.compareTo(obj2);
+		if(result == 0 ){
             System.out.println("The Two Lines Are Equal");
+        } else  if(result > 0) {
+            System.out.println("The Line1 is greater than Line2");
         } else {
-            System.out.println("The Two Lines Are Not Equal");
+        	System.out.println("The Line1 is less than Line2");
         }
-	}
+    }
 
 }
